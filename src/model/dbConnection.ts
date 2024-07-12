15ec1006e44afa018e6f03c0958f84880bb89310
SHIHAD-B <shihadb0@gmail.com>
2024-07-13 04:43:19 +0530

    test:mongoose connection time updated

--- src/model/dbConnection.ts
+++ src/model/dbConnection.ts
@@ -3,7 +3,11 @@ import { DB_URL } from "../config/envConfig";
 
 export default async () => {
     try {
-        await mongoose.connect(String(DB_URL).trim())
+        await mongoose.connect(String(DB_URL).trim(), {
+            serverSelectionTimeoutMS: 5000, 
+            socketTimeoutMS: 45000, 
+        });
+        
         console.log('🍃🍃🍃server connected to the database 🍃🍃🍃')
     } catch (error: any) {
         console.log('⛔⛔⛔failed to connect server to  database⛔⛔⛔')
